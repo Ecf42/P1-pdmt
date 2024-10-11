71e38119005a5f2b82f4af4946316254d9ba2167
Eric Figueiredo <Ericxtk@gmail.com>
2024-10-11 03:05:57 -0300

    refactor(p1-pdmt): reorganiza a ordem da lista e da entrada de lembretes

--- p1-pdmt/src/App.js
+++ p1-pdmt/src/App.js
@@ -9,23 +9,23 @@ export default class App extends Component {
   }
 
   adicionarLembrete = lembrete => {
-      const listaLembretesAtualizada = [...this.state.listaLembretes]
-      listaLembretesAtualizada.push(lembrete)
-      this.setState({ listaLembretes: listaLembretesAtualizada })
+    const listaLembretesAtualizada = [...this.state.listaLembretes]
+    listaLembretesAtualizada.push(lembrete)
+    this.setState({ listaLembretes: listaLembretesAtualizada })
   }
 
   render() {
     return (
       <div>
-        <div className="container border border-success mt-2 py-5">
+        <div className="container border border-warning mt-2 py-5">
           <div className="d-flex justify-content-center">
             <h1>Lembretes</h1>
           </div>
-          <div className="col-12 border border-info py-5">
-            <LembreteLista lista={this.state.listaLembretes} />
-          </div>
-          <div className="col-12 border border-danger mt-2 p-4">
+          <div className="col-12 border border-success mt-2 p-4">
             <LembreteEntrada adicionarLembrete={this.adicionarLembrete} />
+          </div>
+          <div className="col-12 border border-info mt-2 p-4">
+            <LembreteLista lista={this.state.listaLembretes} />
           </div>
         </div>
       </div>
